530672229960fdde7440a4688a9d401074e82105
jungin.rhee <jungin.rhee@cp.kaist.ac.kr>
2023-08-17 03:18:13 +0000

    neotree config

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      print(opts)
+      opts["filesystem"]["filtered_items"] = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      }
+    end,
+  },
+}
